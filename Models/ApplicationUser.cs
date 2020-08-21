3ef1e796d2dec926bf3a96e049a48ce7f9fd2dd8
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-21 10:14:12 -0700

    add identity

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DreamClient.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ Models/DreamClientContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DreamClient.Models
+{
+    public class DreamClientContext : IdentityDbContext<ApplicationUser>
+    {
+      // Don't change code in here.
+    }
+}
